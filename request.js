af26a05638658d8076d659ed5728a8e24a447044
afeena <vremendm@gmail.com>
2015-03-11 15:13:33 +0500

    waterfall

--- request.js
+++ request.js
@@ -9,26 +9,27 @@ var options = {
 };
 var prop = new Array();
 var handler = new htmlparser.DefaultHandler(
-	function (error,dom) {}
+	function (error, dom) {
+	}
 	, {verbose: false, ignoreWhitespace: true}
 );
 
 var event_handler = new htmlparser.DefaultHandler(
-	function (error,dom) {
-		if(error) {
+	function (error, dom) {
+		if (error) {
 			console.log("error" + error);
 		}
-		else{
+		else {
 			prop = [];
 			var title = htmlparser.DomUtils.getElementsByTagName("title", dom);
-			title = htmlparser.DomUtils.getElementsByTagType("text",title);
+			title = htmlparser.DomUtils.getElementsByTagType("text", title);
 			prop.push(title[0].data);
-			var date = htmlparser.DomUtils.getElements({tag_name:"td", class:"date"},dom);
-			date = htmlparser.DomUtils.getElementsByTagType("text",date);
+			var date = htmlparser.DomUtils.getElements({tag_name: "td", class: "date"}, dom);
+			date = htmlparser.DomUtils.getElementsByTagType("text", date);
 			prop.push(date[0].data);
-			var hall = htmlparser.DomUtils.getElementsByTagName("td",dom);
-			hall = htmlparser.DomUtils.getElementsByTagName("a",hall);
-			hall = htmlparser.DomUtils.getElementsByTagType("text",hall);
+			var hall = htmlparser.DomUtils.getElementsByTagName("td", dom);
+			hall = htmlparser.DomUtils.getElementsByTagName("a", hall);
+			hall = htmlparser.DomUtils.getElementsByTagType("text", hall);
 			prop.push(hall[0].data);
 			return prop;
 		}
@@ -39,38 +40,52 @@ var event_handler = new htmlparser.DefaultHandler(
 
 var str = '';
 
+async.waterfall([
+	function (callback) {
+		var events_links;
+		var req = http.request(options, function (res) {
 
-var req = http.request(options, function (res) {
+			res.setEncoding('utf8');
+			res.on('data', function (chunk) {
+				str += chunk;
+			});
 
-	res.setEncoding('utf8');
-	res.on('data', function (chunk) {
-		str += chunk;
-	});
+			res.on('end', function () {
 
-	res.on('end', function () {
+				var parser = new htmlparser.Parser(handler);
+				parser.parseComplete(str);
+				var parsed_html = JSON.stringify(handler.dom, null, 2);
+				var reg = new RegExp('/event/[0-9]+/', 'g');
 
-		var parser = new htmlparser.Parser(handler);
-		parser.parseComplete(str);
-		var parsed_html = JSON.stringify(handler.dom, null, 2);
-		var reg = new RegExp('/event/[0-9]+/', 'g');
+				events_links = parsed_html.match(reg);
+				events_links = events_links.filter(function (elem, pos) {
+					return events_links.indexOf(elem) == pos;
+				});
+				callback(null, events_links);
 
-		var events_links = parsed_html.match(reg);
-		events_links = events_links.filter(function (elem, pos) {
-			return events_links.indexOf(elem) == pos;
+
+			});
+			req.on('error', function (e) {
+				console.log('problem with request: ' + e.message);
+			});
+
+
 		});
+		req.end();
+	},
 
-
-
-		async.forEach(events_links,function(element,callback){
+	function (events_links, callback) {
+		async.forEach(events_links, function (element) {
 			var event_options = {
-				hostname:'www.chel.kassy.ru',
-				path:element
+				hostname: 'www.chel.kassy.ru',
+				path: element
 			}
 
-			var event_req = http.request(event_options,function(res){
+			var event_req = http.request(event_options, function (res) {
 				var events = new Array();
 				var event_page = "";
-				function make_event (link,title,date,hall){
+
+				function make_event(link, title, date, hall) {
 					this.link = link;
 					this.title = title;
 					this.date = date;
@@ -83,16 +98,15 @@ var req = http.request(options, function (res) {
 
 				});
 
-				res.on('end',function(){
+				res.on('end', function () {
 					var event_parser = new htmlparser.Parser(event_handler);
 					event_parser.parseComplete(event_page);
-					var link = event_options.hostname+event_options.path;
-					var event =new  make_event(link,prop[0],prop[1],prop[2]);
+					var link = event_options.hostname + event_options.path;
+					var event = new make_event(link, prop[0], prop[1], prop[2]);
 					events.push(event);
 					console.log(events);
-					callback();
+					//callback(null);
 				});
-
 
 
 			});
@@ -100,16 +114,10 @@ var req = http.request(options, function (res) {
 				console.log('problem with request: ' + e.message);
 			});
 			event_req.end();
+			callback(null);
 
 
 		});
-
-
-	});
-});
-
-req.on('error', function (e) {
-	console.log('problem with request: ' + e.message);
-});
-
-req.end();
+	}], function (err) {
+		console.log("error:"+err);
+})
