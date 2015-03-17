de6b6a747eebcc308232d18c708efe09fbf594ce
afeena <none@none>
2015-03-18 00:38:55 +0500

    all months

--- request.js
+++ request.js
@@ -1,4 +1,4 @@
-var htmlparser = require("htmlparser");
+'use strict';
 var cheerio = require("cheerio");
 var http = require('http');
 var async = require('async');
@@ -6,59 +6,108 @@ var async = require('async');
 
 var options = {
 	hostname: 'www.chel.kassy.ru',
-	path: '/koncerty-i-shou/events/?range=4'
+	path: '/koncerty-i-shou/events/'
 
 };
 console.time('test');
 async.waterfall([
-	function (callback) {
-		var str='';
-		var events_links;
+
+	function(callback){
+		var main_page='';
+
 		var req = http.request(options, function (res) {
 
 			res.setEncoding('utf8');
 			res.on('data', function (chunk) {
-				str += chunk;
+				main_page += chunk;
 			});
 
-			res.on('end', function () {
-
-
-				var reg = new RegExp('/event/[0-9]+/', 'g');
-				events_links = str.match(reg);
-				events_links = events_links.filter(function (elem, pos) {
-					return events_links.indexOf(elem) == pos;
-				});
-				callback(null, events_links);
-
-
-			});
 			req.on('error', function (e) {
 				console.log('problem with request: ' + e.message);
 			});
 
+			res.on('end', function () {
+				var ranges = new Array();
+
+				var $=cheerio.load(main_page);
+				$('.dd').each(function(elem){
+					ranges.push($(this).children('a').attr('href'));
+				});
+
+				callback(null,ranges);
+			});
+
+
+
 
 		});
 		req.end();
+
+	},
+	function (ranges, callback) {
+
+
+		async.map(ranges,function(element,callback) {
+			var str = '';
+			var options = {
+				hostname: 'www.chel.kassy.ru',
+				path: element
+
+			};
+
+			var req = http.request(options, function (res) {
+
+				res.setEncoding('utf8');
+				res.on('data', function (chunk) {
+					str += chunk;
+				});
+
+				res.on('end', function () {
+					var events_links;
+					var reg = new RegExp('/event/[0-9]+/', 'g');
+
+					events_links=str.match(reg);
+
+					if(events_links==null)
+						return callback(null, []);
+
+
+					callback(null, events_links);
+
+
+				});
+				req.on('error', function (e) {
+					console.log('problem with request: ' + e.message);
+				});
+
+
+			});
+			req.end();
+		},function(err,result){
+			var merged=[];
+			var filtered={};
+
+			merged = merged.concat.apply(merged,result);
+			merged.forEach(function(element){
+				filtered[element]=null;
+			});
+			callback(null,Object.keys(filtered));
+		});
 	},
 
 	function (events_links, callback) {
-		async.forEach(events_links, function (element) {
-			var event_options = {
+		async.map(events_links, function (element,callback) {
+
+			var options = {
 				hostname: 'www.chel.kassy.ru',
 				path: element
-			}
 
-			var event_req = http.request(event_options, function (res) {
-				var events = new Array();
+			};
+			var event_req = http.request(options, function (res) {
+
 				var event_page = "";
 
-				function make_event(link, title, date, hall) {
-					this.link = link;
-					this.title = title;
-					this.date = date;
-					this.hall = hall;
-				};
+
 				res.setEncoding('utf8');
 
 				res.on('data', function (chunk) {
@@ -67,18 +116,23 @@ async.waterfall([
 				});
 
 				res.on('end', function () {
-					$=cheerio.load(event_page);
+					var $ = cheerio.load(event_page);
 
-					var date=$('td[class=date]').text();
-					var hall=$('td').children('a').prev().text();
-					var title=$('title').text();
+					var date = $('td[class=date]').first().text();
+					var hall = $('td').children('a').first().text();
+					var title = $('title').text();
 
 
-					var link = event_options.hostname + event_options.path;
-					var event = new make_event(link, title, date, hall);
-					events.push(event);
-					callback(null,events);
-					//console.log(events);
+					var link = options.hostname + options.path;
+
+
+					callback(null, {
+						link: link,
+						title: title,
+						date: date,
+						hall: hall
+					});
+
 
 				});
 
@@ -90,12 +144,11 @@ async.waterfall([
 			event_req.end();
 
 
-
-		});
-	}], function (err,result) {
-		if(err){
-			console.log("error:"+err);
-		}
-		console.log(result);
+		},callback);
+	}], function (err, result) {
+	if (err) {
+		console.log("error:" + err);
+	}
+	console.log(result);
 	console.timeEnd('test');
-})
+});
